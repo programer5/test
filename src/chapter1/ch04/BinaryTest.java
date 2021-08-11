19b2717447323d239a275b14b546edcc482889ac
minseo23 <neverdie4757@gmail.com>
2021-08-12 00:05:33 +0900

    컴퓨터에서 자료 표현하기

---
+++ src/chapter1/ch04/BinaryTest.java
@@ -0,0 +1,17 @@
+package chapter1.ch04;
+
+public class BinaryTest {
+
+    public static void main(String[] args) {
+
+        int num = 10;
+        int bNum = 0B1010;
+        int oNum = 012;
+        int xNum = 0XA;
+
+        System.out.println("num = " + num);
+        System.out.println("bNum = " + bNum);
+        System.out.println("oNum = " + oNum);
+        System.out.println("oNum = " + oNum);
+    }
+}
