7674c996eceea09d7f1717cbe32ce6a57d0ca1af
minseo23 <neverdie4757@gmail.com>
2021-08-24 23:09:15 +0900

    변하지 않는 상수와 리터럴, 변수의 형 변환

---
+++ src/chapter1/ch10/TypeConversionTest.java
@@ -0,0 +1,29 @@
+package chapter1.ch10;
+
+public class TypeConversionTest {
+
+    public static void main(String[] args) {
+
+//        byte bNum = 125;
+        int iNum = 255;
+        byte bNum = (byte) iNum;
+
+        System.out.println(bNum);
+        System.out.println("iNum = " + iNum);
+
+        double dNum = 3.14;
+        int iNum2 = (int) dNum;
+
+        System.out.println(dNum);
+        System.out.println("iNum2 = " + iNum2);
+
+        double dnum2 = 1.2;
+        float fnum = 0.9f;
+
+        int iNum3 = (int) dnum2 + (int) fnum;
+        int iNum4 = (int) (dnum2 + fnum);
+
+        System.out.println(iNum3);
+        System.out.println("iNum4 = " + iNum4);
+    }
+}
