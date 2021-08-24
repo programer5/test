2693f9f1f55ce629c177e95da1f4d14bdf6a8a47
minseo23 <neverdie4757@gmail.com>
2021-08-25 00:25:25 +0900

    STACK 구현

---
+++ src/chapter5/ch04/MyArrayStack.java
@@ -0,0 +1,67 @@
+package chapter5.ch04;
+
+import chapter5.ch02.MyArray;
+
+public class MyArrayStack {
+
+    MyArray arrayStack;
+    int top;
+
+    public MyArrayStack() {
+        top = 0;
+        arrayStack = new MyArray();
+    }
+
+    public MyArrayStack(int size) {
+        top = 0;
+        arrayStack = new MyArray(size);
+    }
+
+    public void push(int data) {
+        if (isFull()) {
+            System.out.println("stack is full");
+            return;
+        }
+        arrayStack.addElement(data);
+        top++;
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("stack is empty");
+            return MyArray.ERROR_NUM;
+        }
+
+        return arrayStack.removeElement(--top);
+    }
+
+//    public int peak() {
+//        if (isEmpty()) {
+//            System.out.println("stack is empty");
+//            return MyArray.ERROR_NUM;
+//        }
+//
+//        return arrayStack.getElement(--top);
+//    }
+
+    public boolean isFull() {
+        if (top == arrayStack.ARRAY_SIZE) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isEmpty() {
+        if (top == 0) {
+            System.out.println("stack is empty");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+//    public void printAll() {
+//        arrayStack.printAll();
+//    }
+}
---
+++ src/chapter5/ch04/MyArrayStackTest.java
@@ -0,0 +1,15 @@
+package chapter5.ch04;
+
+public class MyArrayStackTest {
+
+    public static void main(String[] args) {
+
+        MyArrayStack stack = new MyArrayStack();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+
+    }
+
+}
