4545496bfbb97e4706ded423767c462f8733b40e
minseo23 <neverdie4757@gmail.com>
2021-08-31 00:25:18 +0900

    추상 클래스 의미와 구현하는 방법

---
+++ src/chapter2/ch10/BirthDayTest.java
@@ -0,0 +1,14 @@
+package chapter2.ch10;
+
+public class BirthDayTest {
+
+    public static void main(String[] args) {
+
+        BirthDay birthDay = new BirthDay();
+        birthDay.setYear(2021);
+        birthDay.setMonth(12);
+        birthDay.setDay(30);
+
+        birthDay.showDate();
+    }
+}
---
+++ src/chapter3/ch09/ComputerTest.java
@@ -0,0 +1,10 @@
+package chapter3.ch09;
+
+public class ComputerTest {
+
+    public static void main(String[] args) {
+
+        Computer desktop = new DeskTop();
+        desktop.display();
+    }
+}
