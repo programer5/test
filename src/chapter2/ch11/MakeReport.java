fc48b18fc4658a289fa7596779dbbb61e0fe78a9
minseo23 <neverdie4757@gmail.com>
2021-08-31 23:36:48 +0900

    캡슐화(encapsulation)

---
+++ src/chapter2/ch11/MakeReport.java
@@ -0,0 +1,36 @@
+package chapter2.ch11;
+
+public class MakeReport {
+
+    StringBuffer buffer = new StringBuffer();
+
+    private String line = "================================\n";
+    private String title = "  이름\t   주소 \t\t  전화번호  \n";
+
+    private void makeHeader() {
+        buffer.append(line);
+        buffer.append(title);
+        buffer.append(line);
+    }
+
+    private void generateBody() {
+        buffer.append("James \t");
+        buffer.append("Seoul Korea \t");
+        buffer.append("010-1234-5678 \n");
+
+        buffer.append("Tomas \t");
+        buffer.append("NewYork \t");
+        buffer.append("010-1111-2222 \t");
+    }
+
+    private void makeFooter() {
+        buffer.append(line);
+    }
+
+    public String getReport() {
+        makeHeader();;
+        generateBody();
+        makeFooter();
+        return buffer.toString();
+    }
+}
