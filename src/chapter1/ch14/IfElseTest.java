31f2916dc19f8ee61a4326e811ce7f729016ab70
minseo23 <neverdie4757@gmail.com>
2021-08-31 23:21:54 +0900

    if문

---
+++ src/chapter1/ch14/IfElseTest.java
@@ -0,0 +1,15 @@
+package chapter1.ch14;
+
+public class IfElseTest {
+
+    public static void main(String[] args) {
+        
+        int age = 6;
+        
+        if (age >= 8) {
+            System.out.println("학교에 다닙니다.");
+        } else {
+            System.out.println("노는게 젤 좋아");
+        }
+    }
+}
