1e37e74761c962f782be814354007f240bfcd607
minseo23 <neverdie4757@gmail.com>
2021-09-10 23:50:24 +0900

    for 문

---
+++ src/chapter1/ch19/ForTest.java
@@ -0,0 +1,14 @@
+package chapter1.ch19;
+
+public class ForTest {
+
+    public static void main(String[] args) {
+
+        int count = 1;
+        int sum = 0;
+
+        for (int i = 0; i < 10; count++) {
+            sum += count;
+        }
+    }
+}
